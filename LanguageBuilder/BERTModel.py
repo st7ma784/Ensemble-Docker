ca2c825da27470bf159dcd85ee36704201c8ed5f
st7ma784 <s.mander@lancaster.ac.uk>
2020-07-29 00:05:19 +0100

    remove OO approach, works

--- LanguageBuilder/BERTModel.py
+++ LanguageBuilder/BERTModel.py
@@ -9,85 +9,61 @@ from time import time
 from gensim import utils
 from unidecode import unidecode
 from itertools import repeat
-from multiprocessing import Pool, freeze_support
+from multiprocessing import Pool, freeze_support, cpu_count
 from gensim.summarization.textcleaner import tokenize_by_word
 import copy
-from langdetect import detect, lang_detect_exception
-import concurrent.futures as cf 
-class WordTrainer(object):
-    def __init__(self, dir_name="textfiles",):
-        self.dir_name = dir_name
-        self.filenames= list(filter(lambda fname: fname.endswith('.txt'), os.listdir(dir_name)))
-        print("Found Files: \n " + "\n".join(self.filenames))
-        self.files={}
-        self.knownlanguages=set()
-    def testLanguage(self,text):
-        try:
-            language=detect(text)#.lang#
-        except lang_detect_exception.LangDetectException as e:
-            language='en'
-        if language not in self.knownlanguages:
-            self.knownlanguages.add(language)
-        return language
-    def openfile(self, file_name):
-        if self.files.get(file_name,None) is None:
-
-            with open(os.path.join(self.dir_name, file_name),'r',encoding="utf",errors="ignore") as text:
-                print("Opened : " + file_name)
-                sentences= gensim.summarization.textcleaner.split_sentences(text.read())
-                self.files[file_name]=sentences
-        sentences=filter(lambda sentence:len(sentence.split())>4,self.files[file_name])
-        return [{"text":sentence, "lang":self.testLanguage(sentence)} for sentence in sentences]
-    def __iter__(self):
-    	
-        with cf.ProcessPoolExecutor(max_workers=os.cpu_count()) as tp:
-            fl = [tp.submit(self.openfile, fn) for fn in self.filenames]
-        for sentencelist in cf.as_completed(fl):
-            for sentence in sentencelist.result():
-                yield sentence
-texts=None
-def make_model():
-    global texts
-    print("Reading Vocab")
-    t = time()
-    print("Reading sentences from files...")
-    if texts is None:
-    	texts=WordTrainer()
-
-    #Copora=gensim.corpora.textcorpus.TextCorpus(input="./test/")
-
-    print('Time to train model on everything: {} mins'.format(round((time() - t) / 60, 2)))
-    return texts
 
 from collections import Counter
 from functools import partial
 import pymongo
-def Connect(URL):
+from langdetect import detect, lang_detect_exception
+import concurrent.futures as cf 
+from multiprocessing import Pool
+
+
+
+def Connect(URL,collection="sentences"):
 
   mongo=pymongo.MongoClient(URL)
   db= mongo["Connects"] #Connect to DB
   col=db["Access"]
   col.insert_one({"time":datetime.now()})
-  db=mongo["sentences"]
+  db=mongo[collection]
   return db
 
 
 URL= os.environ.get("MONGO_CLUSTERURI")
-Words={}
-languages=set(['en'])
+knownlanguages=set(['en'])
 def WriteLanguages(languages):
 	Connect(URL,"LANGUAGES")["CODES"].insert_many(dict({"code":code}) for code in list(languages))
-
+def testLanguage(text):
+    global knownlanguages
+    try:
+        language=detect(text)#.lang#
+    except lang_detect_exception.LangDetectException as e:
+        language='UNK'
+    if language not in knownlanguages:
+        knownlanguages.add(language)
+    return language
+start=time()
+def openfile(file_name):
+    global start
+    t=time()
+    
+    DB=Connect(URL)["Sentences"]
+    with open(file_name,'r',encoding="utf",errors="ignore") as text:
+        print("Opened : " + file_name)
+        sentences= gensim.summarization.textcleaner.split_sentences(text.read())
+        sentences=filter(lambda sentence:len(sentence.split())>4,sentences)
+        DB.insert_many([{"text":sentence, "lang":testLanguage(sentence)} for sentence in sentences])
+        #return [{"text":sentence, "lang":self.testLanguage(sentence)} for sentence in sentences]
+        print('Time to upload{}: {} mins / {} mins'.format(file_name,round((time() - t) / 60, 2),round((time() - start) / 60, 2) ))
+        return 1
 def main():
-    print("creating file list : " )
-    samples=make_model()
-    print("Pushing to database:")
-    LanguageDB=Connect(URL)
-    print("connected to database")
-    Sentences=LanguageDB["Sentences"]
-    Sentences.insert_many(list([sentence for sentence in samples]))
-    print("Languages found :" + list(samples.knownlanguages))
-    WriteLanguages(list(samples.knownlanguages))
+    dir_name="textfiles"
+    filenames=[os.path.join(dir_name,filename) for filename in filter(lambda fname: fname.endswith('.txt'), os.listdir(dir_name))]
+    with Pool(cpu_count()) as p:
+        p.map(openfile, filenames)
 if __name__=="__main__":
     freeze_support()
     main()
